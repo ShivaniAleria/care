9355f7f74529cd3297dbec05d7ab7f3bd35b72a0
NJSIT <NJSIT@DESKTOP-52SANI8>
2018-09-12 18:06:14 +0530

    Add project files.

---
+++ TrialWebApplication/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrialWebApplication.Models;
+
+namespace TrialWebApplication.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()  {Name = "Shrek!" };
+
+            var customers = new List<Customer> {
+               new Customer { Name ="Customer 1"},
+               new Customer { Name = "Customer 2"}
+           };
+
+            var viewModel = new RandomMovieViewModel
+            {
+
+            }
+
+            return View(movie);
+            //return Content("hii");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home");
+        }
+
+        //public ActionResult Edit(int id) {
+        //    return Content("id=" + id);
+        //}
+
+        //public ActionResult Index(int? PageIndex, string sortBy)
+        //{
+        //    if (!PageIndex.HasValue)
+        //        PageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("PageIndex={0}&sortBy={1}",PageIndex,sortBy));
+
+        //}
+
+        [Route("movies/released/{year}/{month;regex(\\d{4}):range(1.12)}")]
+        public ActionResult ByRealeaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ TrialWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrialWebApplication.Startup))]
+namespace TrialWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
